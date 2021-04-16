3fba58e219901d9e4ad88745763d52116cb20057
Carson Farmer <carson.farmer@gmail.com>
2021-04-15 21:59:14 -0700

    feat: support sha256 hash of header+payload
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- jwt.go
+++ jwt.go
@@ -1,21 +1,31 @@
 package eddsa
 
 import (
+	"crypto/sha256"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/libp2p/go-libp2p-core/crypto"
 )
 
 // SigningMethodEd25519 implements the Ed25519 signing method.
-// Expects *crypto.Ed25519PublicKey for signing and *crypto.Ed25519PublicKey for validation.
+// Expects *crypto.Ed25519PublicKey for signing and *crypto.Ed25519PublicKey
+// for validation.
+// The signing method can be optionally configured to sign and validate the
+// sha256 hash of the input string.
 type SigningMethodEd25519 struct {
 	Name string
+	Hash bool
 }
 
 // SigningMethodEd25519i is a specific instance for Ed25519.
 var SigningMethodEd25519i *SigningMethodEd25519
 
+// SigningMethodEd25519Sha is a specific instance for Ed25519 with hashing enabled
+var SigningMethodEd25519Sha *SigningMethodEd25519
+
 func init() {
-	SigningMethodEd25519i = &SigningMethodEd25519{"EdDSA"}
+	SigningMethodEd25519i = &SigningMethodEd25519{"EdDSA", false}
+	SigningMethodEd25519Sha = &SigningMethodEd25519{"EdDSA", true}
 	jwt.RegisterSigningMethod(SigningMethodEd25519i.Alg(), func() jwt.SigningMethod {
 		return SigningMethodEd25519i
 	})
@@ -44,6 +54,15 @@ func (m *SigningMethodEd25519) Verify(signingString, signature string, key inter
 		return jwt.ErrInvalidKeyType
 	}
 
+	if m.Hash {
+		h := sha256.New()
+		_, err := h.Write([]byte(signingString))
+		if err != nil {
+			return err
+		}
+		signingString = string(h.Sum(nil))
+	}
+
 	// verify the signature
 	valid, err := ed25519Key.Verify([]byte(signingString), sig)
 	if err != nil {
@@ -65,6 +84,15 @@ func (m *SigningMethodEd25519) Sign(signingString string, key interface{}) (stri
 	// validate type of key
 	if ed25519Key, ok = key.(*crypto.Ed25519PrivateKey); !ok {
 		return "", jwt.ErrInvalidKey
+	}
+
+	if m.Hash {
+		h := sha256.New()
+		_, err := h.Write([]byte(signingString))
+		if err != nil {
+			return "", err
+		}
+		signingString = string(h.Sum(nil))
 	}
 
 	sigBytes, err := ed25519Key.Sign([]byte(signingString))
--- jwt_test.go
+++ jwt_test.go
@@ -18,6 +18,7 @@ var ed25519TestData = []struct {
 	alg         string
 	claims      map[string]interface{}
 	valid       bool
+	hash        bool
 }{
 	{
 		"EdDSA",
@@ -25,6 +26,7 @@ var ed25519TestData = []struct {
 		"EdDSA",
 		map[string]interface{}{"jti": "foo", "sub": "bar"},
 		true,
+		false,
 	},
 	{
 		"invalid key",
@@ -32,6 +34,15 @@ var ed25519TestData = []struct {
 		"EdDSA",
 		map[string]interface{}{"jti": "foo", "sub": "bar"},
 		false,
+		false,
+	},
+	{
+		"sha256 hash for signature",
+		"eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmb28iLCJzdWIiOiJiYXIifQ.x1tRfmjfL70P7jxXHTfW47D9sjwbl0BGJECjIfocntlQi-ACVF8OuaW829f-5ag6HBt_E76Eh8IkLXeBK-gsBA",
+		"EdDSA",
+		map[string]interface{}{"jti": "foo", "sub": "bar"},
+		true,
+		true,
 	},
 }
 
@@ -49,7 +60,12 @@ func TestSigningMethodEd25519_Sign(t *testing.T) {
 	for _, data := range ed25519TestData {
 		if data.valid {
 			parts := strings.Split(data.tokenString, ".")
-			method := jwt.GetSigningMethod(data.alg)
+			var method jwt.SigningMethod
+			if data.hash {
+				method = eddsa.SigningMethodEd25519Sha
+			} else {
+				method = jwt.GetSigningMethod(data.alg)
+			}
 			sig, err := method.Sign(strings.Join(parts[0:2], "."), sk)
 			if err != nil {
 				t.Errorf("[%v] error signing token: %v", data.name, err)
@@ -69,7 +85,12 @@ func TestSigningMethodEd25519_Verify(t *testing.T) {
 	for _, data := range ed25519TestData {
 		parts := strings.Split(data.tokenString, ".")
 
-		method := jwt.GetSigningMethod(data.alg)
+		var method jwt.SigningMethod
+		if data.hash {
+			method = eddsa.SigningMethodEd25519Sha
+		} else {
+			method = jwt.GetSigningMethod(data.alg)
+		}
 		err := method.Verify(strings.Join(parts[0:2], "."), parts[2], pk)
 		if data.valid && err != nil {
 			t.Errorf("[%v] error while verifying key: %v", data.name, err)
@@ -90,6 +111,11 @@ func TestGenerateEd25519Token(t *testing.T) {
 		Subject: "bar",
 	}
 	_, err = jwt.NewWithClaims(eddsa.SigningMethodEd25519i, claims).SignedString(sk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = jwt.NewWithClaims(eddsa.SigningMethodEd25519Sha, claims).SignedString(sk)
 	if err != nil {
 		t.Fatal(err)
 	}
